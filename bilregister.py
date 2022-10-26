e89658b21373a5e98e111d006cba7cce921caee3
HjalmarAlbinsson <116351017+HjalmarAlbinsson@users.noreply.github.com>
2022-10-26 14:44:16 +0200

    Added the "sortlist" section
    Added the "sortlist" section that was requested in the assignment. Also reworked the "get" functions to return the value instead of printing the value. Then changed the aforementioned function calls to print the value.

--- bilregister.py
+++ bilregister.py
@@ -16,7 +16,7 @@ class Car():
         '''
         Skriver ut bilmärket
         '''
-        print(self.brand)
+        return(self.brand)
 
     def set_brand(self, new_brand):
         '''
@@ -28,11 +28,13 @@ class Car():
     def set_color(self, new_color):
         self.color = new_color
     def get_color(self):
-        print(self.color)
+        return(self.color)
     def set_mileage(self, new_mileage):
         self.mileage = new_mileage
     def get_mileage(self):
         return(self.mileage)
+    def get_modelyear(self):
+        return(self.modelyear)
     
 
 
@@ -41,11 +43,11 @@ class Car():
 # Dessa objekt har också tillgång till klassens metoder (funktioner).
 a_car = Car('Volvo', 'Blå', 1587, 1999)
 b_car = Car("Mercedez","Röd", 1934, 1998)
-a_car.get_brand()
-b_car.get_brand()
-a_car.get_color()
+print(a_car.get_brand())
+print(b_car.get_brand())
+print(a_car.get_color())
 a_car.set_color("Grön")
-a_car.get_color()
+print(a_car.get_color())
 the_mileage = a_car.get_mileage()
 print(the_mileage)
 c_car = Car("Lamborghini", "Gul", 34, 2016)
@@ -54,8 +56,34 @@ my_cars = [a_car, b_car, c_car, d_car]
 for car in my_cars:
     print(car.get_brand())
 
-print("A-Z(1), Miltal(2), Årsmodel(3), Färg(4)")
-answer = input("Sorterings val: -> ")
-if answer == "1":
-    for car in my_cars:
-        
+while 1 == 1:
+    print("A-Z(1), Miltal(2), Årsmodel(3), Färg(4), Quit(q)")
+    answer = input("Sorterings val: -> ")
+    sort_list = []
+    if answer == "1":
+        for car in my_cars:
+            sort_list.append(car.get_brand())
+        sort_list.sort()
+        for brand in sort_list:
+            print(brand)
+    elif answer == "2":
+        for car in my_cars:
+            sort_list.append(car.get_mileage())
+        sort_list.sort()
+        for mileage in sort_list:
+            print(mileage)
+    elif answer == "3":
+        for car in my_cars:
+            sort_list.append(car.get_modelyear())
+        sort_list.sort()
+        sort_list.reverse()
+        for modelyear in sort_list:
+            print(modelyear)
+    elif answer == "4":
+        for car in my_cars:
+            sort_list.append(car.get_color())
+        sort_list.sort()
+        for color in sort_list:
+            print(color)
+    elif answer == "q":
+        break
